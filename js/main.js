9fc7a66e5cf701f960b37113209933bab67d2da9
Oched <oched.denis@gmail.com>
2018-12-29 00:27:28 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+function main() {
+	$('.txt').hide();
+	
+	$('.first').hide();
+
+	$('.second').hide();
+
+	$('.third').hide();
+
+	$('.fourth').hide();
+
+	$('.fifth').hide();
+
+	$('.txt').fadeIn(5000);
+
+	$('.first').fadeIn(5000);
+
+	$('.second').fadeIn(8000);
+
+	$('.third').fadeIn(10000);
+
+	$('.fourth').fadeIn(15000);
+
+	$('.fifth').fadeIn(20000);
+};
+
+
+$(document).ready(main);
